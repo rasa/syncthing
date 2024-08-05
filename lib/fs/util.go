7d191a85884a0d98107709c4de8ca5a9cd3d38c4
Ross Smith II <ross@smithii.com>
2024-08-05 08:25:52 -0700

    Fix typo

--- lib/fs/util.go
+++ lib/fs/util.go
@@ -52,8 +52,8 @@ const windowsDisallowedCharacters = (`<>:"|?*` +
 	"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
 	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f")
 
-// WindowsInvalidFilename returns an error if name is invalid for the specified
-// encoder. Uses lexical analysis only.
+// WindowsInvalidFilename returns an error if name is invalid on Windows.
+// Uses lexical analysis only.
 func WindowsInvalidFilename(name string, allowReservedFilenames bool) error {
 	err := windowsInvalidFilenameChars(name)
 	if err != nil {

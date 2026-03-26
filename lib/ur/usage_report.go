96ff91ca730f1b4124f4f5f61d3b1cccc6c97d9e
Ross Smith II <ross@smithii.com>
2026-03-25 20:32:17 -0700

    feat: Add folder case-sensitivity support on Windows
    Fixes #5621
    
    Signed-off-by: Ross Smith II <ross@smithii.com>

--- lib/ur/usage_report.go
+++ lib/ur/usage_report.go
@@ -282,6 +282,9 @@ func (s *Service) reportData(ctx context.Context, urVersion int, preview bool) (
 			if !cfg.BlockIndexing {
 				report.FolderUsesV3.NoBlockIndexing++
 			}
+			if cfg.CaseSensitiveDir {
+				report.FolderUsesV3.CaseSensitiveDir++
+			}
 		}
 		slices.Sort(report.FolderUsesV3.FsWatcherDelays)
 
